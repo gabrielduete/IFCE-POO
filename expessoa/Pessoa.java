e244becf3c4cbe3069038eb30404df2c57010488
Gabriel Duete <gabrielmonteiroduete@gmail.com>
2021-09-25 21:21:51 -0300

    Algumas auterações

--- expessoa/Pessoa.java
+++ expessoa/Pessoa.java
@@ -1,5 +1,10 @@
 package expessoa;
 
 public class Pessoa {
+    @Override
+    public boolean equals(Object obj) {
+        // TODO Auto-generated method stub
+        return super.equals(obj);
+    }
     
 }
