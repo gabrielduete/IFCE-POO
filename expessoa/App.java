3bb8b9fea173ecb30cfa8a9b637d788ab9000434
Gabriel Duete <gabrielmonteiroduete@gmail.com>
2021-09-27 17:43:48 -0300

    Exercício Pessoa Java

--- expessoa/App.java
+++ expessoa/App.java
@@ -1,10 +1,10 @@
 package expessoa;
 
 public class App {
-    public static void main(String[] args) {        
-        Pessoa p1 = new Pessoa("José", "10000000");
-        Pessoa p2 = new Pessoa("José", "10000000");
+    public static void main(String[] args) {
+        Pessoa p1 = new Pessoa("100", "Gabriel");
+        Pessoa p2 = new Pessoa("100", "Gabriel");
 
         System.out.println(p1.equals(p2));
-   }    
+    }
 }
